ef390552ecf0139b84e98c984bc3da2380e03c0a
Shampooh <agent.serega@yandex.ru>
2020-03-01 01:29:29 +0300

    Fix issue with dataset_downloader_

--- dataset/dataset_dowloader_.py
+++ dataset/dataset_dowloader_.py
@@ -1,6 +1,7 @@
+import  os
 from modelarts.session import Session
 
 def div2k_dowloader():
     session = Session()
-    bucket_path="/cv-course-public/coding-3/high\ resolution\ images/DIV2K_train_HR.zip"
+    bucket_path=os.path.join('.', '/cv-course-public/coding 3/datasets/high resolution images/', 'DIV2K_train_HR.zip')
     session.download_data(bucket_path=bucket_path, path="./data/DIV2K_train_HR.zip")
